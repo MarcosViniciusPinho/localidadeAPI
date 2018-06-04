e8224fac4b70c8015702d220785de051c8b75348
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-06-03 22:29:13 -0300

    Subir aplicação para o Heroku

--- src/main/java/br/com/localidade/LocalidadeApiApplication.java
+++ src/main/java/br/com/localidade/LocalidadeApiApplication.java
@@ -4,39 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class LocalidadeApiApplication {
-
-//	@Autowired
-//	private Environment env;
-//
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource ds = new DriverManagerDataSource();
-//		ds.setDriverClassName(env.getProperty("com.mysql.jdbc.Driver"));
-//		ds.setUrl(env.getProperty("DATABASE_URL"));
-//		ds.setUsername(env.getProperty("DATABASE_USER"));
-//		ds.setPassword(env.getProperty("DATABASE_PASSWORD"));
-//		return ds;
-//	}
-
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource ds = new DriverManagerDataSource();
-//		ds.setDriverClassName("com.mysql.jdbc.Driver");
-//		ds.setUrl("jdbc:mysql://nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lw9a8evt3wv574zu");
-//		ds.setUsername("qxhvj7ae5h4ny8ep");
-//		ds.setPassword("wlezgn7tlylw7wgf");
-//		return ds;
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalidadeApiApplication.class, args);
 	}
-
-//	@Bean
-//	public DataSource dataSource() {
-//		return DataSourceBuilder.create().build();
-//	}
 
 }
