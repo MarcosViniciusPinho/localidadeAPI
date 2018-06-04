ee7ed2a14216c6b9190f1d12d82db2aca9f3aca1
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-06-03 22:15:17 -0300

    Subir aplicação para o Heroku

--- src/main/java/br/com/localidade/LocalidadeApiApplication.java
+++ src/main/java/br/com/localidade/LocalidadeApiApplication.java
@@ -1,11 +1,30 @@
 package br.com.localidade;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class LocalidadeApiApplication {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(env.getProperty("com.mysql.jdbc.Driver"));
+		ds.setUrl(env.getProperty("DATABASE_URL"));
+		ds.setUsername(env.getProperty("DATABASE_USER"));
+		ds.setPassword(env.getProperty("DATABASE_PASSWORD"));
+		return ds;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalidadeApiApplication.class, args);
