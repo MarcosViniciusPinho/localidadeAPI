82a31710c2a1abe005839ab9f3a86fea848a6369
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-06-03 22:05:48 -0300

    Subir aplicação para o Heroku

--- src/main/java/br/com/localidade/LocalidadeApiApplication.java
+++ src/main/java/br/com/localidade/LocalidadeApiApplication.java
@@ -1,12 +1,12 @@
 package br.com.localidade;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.DependsOn;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@DependsOn("appConfig")
 public class LocalidadeApiApplication {
 
 	public static void main(String[] args) {
